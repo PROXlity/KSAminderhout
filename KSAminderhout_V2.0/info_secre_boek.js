01ef2a9222f1af60b0b54f72f3ab34a8460d940b
PROXlity <32112479+PROXlity@users.noreply.github.com>
2017-09-20 00:04:05 +0200

    Add files via upload

---
+++ KSAminderhout_V2.0/info_secre_boek.js
@@ -0,0 +1,19 @@
+﻿//secretariaat
+var secrenaam = document.getElementById("SECRENAAM");
+secrenaam.textContent = "Dorien Mertens";//<---naam
+var secreadres = document.getElementById("SECREADRES");
+secreadres.textContent = "Gemeentestraat 16, 2322 Minderhout";//<---adres
+var secretel = document.getElementById("SECRETEL");
+secretel.textContent = "0470/62 87 43";//<---gsmnummer
+var secremail = document.getElementById("SECREMAIL");
+secremail.textContent = "mertens.dorien@hotmail.be";//<---mailadres
+
+//boekhouder
+var boeknaam = document.getElementById("BOEKNAAM");
+boeknaam.textContent = "Yana Vermeiren";//<---naam
+var boekadres = document.getElementById("BOEKADRES");
+boekadres.textContent = "Bredaseweg 42A, 2322 Minderhout";//<---adres
+var boektel = document.getElementById("BOEKTEL");
+boektel.textContent = "0474/86 44 13";//<---gsmnummer
+var boekmail = document.getElementById("BOEKMAIL");
+boekmail.textContent = "yana.vermeiren@hotmail.com";//<---mailadres
